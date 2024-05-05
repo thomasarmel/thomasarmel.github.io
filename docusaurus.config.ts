0946e09d05bee8d98bb3d1cb4d9e108bb1cfc2c1
Thomas Prévost <thomas.prevost@etu.univ-cotedazur.fr>
2024-05-05 17:00:38 +0200

    Add deployment branch

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,6 +17,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'thomasarmel', // Usually your GitHub org/user name.
   projectName: 'thomasarmel.github.io', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
